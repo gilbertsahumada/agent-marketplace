b47269d97e8b58189d384963a3479c51f8b586d6
Gilberts Ahumada <gilbertsahumada@gmail.com>
2026-08-24 03:11:30 +0200

    submission: src/business/composition.ts

--- src/business/composition.ts
+++ src/business/composition.ts
@@ -1,7 +1,11 @@
 import {
   erc8183SpikeRepository,
+  mainnetErc8183Repository,
+  mainnetBrowserDemoConfigRepository,
+  mainnetJobProofRepository,
   marketplaceAgentRepository,
   publicJobProofRepository,
+  publicVerificationRepository,
 } from "../data/composition.js";
 import { CompareMarketplaceAgents } from "./use-cases/compare-marketplace-agents.js";
 import { GetMarketplaceAgent } from "./use-cases/get-marketplace-agent.js";
@@ -12,6 +16,10 @@ import { NotifyFundedJob } from "./use-cases/notify-funded-job.js";
 import { PrepareErc8183Hire } from "./use-cases/prepare-erc8183-hire.js";
 import { RequestErc8183Quote } from "./use-cases/request-erc8183-quote.js";
 import { GetErc8183TestnetJobTracking } from "./use-cases/get-erc8183-testnet-job-tracking.js";
+import { GetPublicVerificationSnapshot } from "./use-cases/get-public-verification-snapshot.js";
+import { GetMainnetBrowserDemoConfig } from "./use-cases/get-mainnet-browser-demo-config.js";
+import { GetMainnetJobProof } from "./use-cases/get-mainnet-job-proof.js";
+import { GetMarketplaceLandingCatalog } from "./use-cases/get-marketplace-landing-catalog.js";
 
 export const listMarketplaceAgents = new ListMarketplaceAgents(marketplaceAgentRepository);
 export const getMarketplaceAgent = new GetMarketplaceAgent(marketplaceAgentRepository);
@@ -25,3 +33,11 @@ export const getErc8183TestnetJobTracking = new GetErc8183TestnetJobTracking(
   erc8183SpikeRepository,
   publicJobProofRepository,
 );
+export const requestMainnetErc8183Quote = new RequestErc8183Quote(mainnetErc8183Repository);
+export const prepareMainnetErc8183Hire = new PrepareErc8183Hire(mainnetErc8183Repository);
+export const notifyMainnetFundedJob = new NotifyFundedJob(mainnetErc8183Repository);
+export const getMainnetErc8183JobStatus = new GetErc8183JobStatus(mainnetErc8183Repository);
+export const getPublicVerificationSnapshot = new GetPublicVerificationSnapshot(publicVerificationRepository);
+export const getMarketplaceLandingCatalog = new GetMarketplaceLandingCatalog(getPublicVerificationSnapshot);
+export const getMainnetBrowserDemoConfig = new GetMainnetBrowserDemoConfig(mainnetBrowserDemoConfigRepository);
+export const getMainnetJobProof = new GetMainnetJobProof(mainnetJobProofRepository);
